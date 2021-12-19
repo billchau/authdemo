10d0accc9aa32b55d5d1b5bdd4265bca85930a39
billchau <acebillc@gmail.com>
2021-12-19 18:01:51 +0800

    Initial Commit
    Initial Commit

---
+++ src/main/kotlin/com/billchau/authdemo/authdemo/AuthdemoApplication.kt
@@ -0,0 +1,11 @@
+package com.billchau.authdemo.authdemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AuthdemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<AuthdemoApplication>(*args)
+}
